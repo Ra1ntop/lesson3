82c023c3b3933114eab32a411c96578c8633dda0
Ra1ntop <man111play@gmail.com>
2022-02-14 16:30:57 +0200

    first commit

---
+++ 1.py
@@ -0,0 +1,65 @@
+a = 5
+b = 8
+c = 7 
+print("Обьекты ", a, b, c, sep=" ***** ")
+
+a = 5
+b = 8
+c = 7 
+print( a, b, c, sep=" ***** ", end=":")
+
+#message = input(" Привет, меня зовут bot, как тебя зовут ?")
+#print("Hello, ", message) 
+
+
+
+name = input("Please enter your name: ")
+a = f"Игра началась, рад приветствовать тебя - {name}"
+print(a)
+
+
+#message = input(" Привет, меня зовут bot, как тебя зовут ?")
+#print("Hello, ", message) 
+
+
+
+#name = input("Please enter your name: ")
+#a = f"Игра началась, рад приветствовать тебя - {name}"
+#print(f"Игра началась {name}, рад приветствовать тебя - {name}")
+f= 7
+f+=3
+print(f)
+f-=4
+print(f)
+f*=3
+print(f)
+f/=10
+print(f)
+
+r = "Den"
+r += "is"
+print(r)
+
+t = " Нажмите пожалуйста enter, для того, чтобы продолжить." 
+t += "\nКак Вас зовут ?" 
+name = input(t) 
+print(f"\nПривет, {name}!")
+
+q = 20**2
+print(q)
+
+p= pow(10, 2)
+print(p)
+
+
+import math
+t = math.pi
+#b = int(input("Какой факториал можно вычислить ? "))
+f = math.sin(t/6)
+print(f)
+
+
+import random
+q = [1,8,9,10,4,5,8]
+rand = random.choice(q)
+print(rand)
